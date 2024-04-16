85f6203cc94c19c9a9a0f527c08e2ba32bd25e4c
thebrainfox <thebrainfoxytb44@gmail.com>
2024-04-16 09:54:26 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,3 +4,20 @@ constructor(lang: string = 'en', auto: bool = false) {
 this.auto = auto
 }
 }
+
+export class CreateLocalize() {
+constructor(str: string, lang: string, value: string) {
+this.str = str
+this.lang = lang
+this.value = value
+}
+add(lang, value) {
+
+}
+private register(local: array, default: string)
+if (!local[default]) throw new Error("default language has no value")
+const locals = local.map((e,i) => {
+return `  ${i} = ${e},`
+})
+const content = "default export enum(\n" + locals.join("\n")
+}
